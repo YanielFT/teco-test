c6d25d4b53faf7aaea1514d5549aba65b266ee19
YanielFT <ceyfuentes@gmail.com>
2025-04-08 03:10:31 -0400

    refactor: Clean up CheckoutForm and move clearCart logic to CheckoutComplete

--- src/components/Checkout/CheckoutForm.tsx
+++ src/components/Checkout/CheckoutForm.tsx
@@ -3,10 +3,6 @@ import { useForm } from "react-hook-form";
 import { CheckoutSchema } from "./Checkout.schema";
 import { InputField } from "../ui/InputField";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-import { clearCart } from "../../store/Cart/CartSlice";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../store/store";
 
 type FormData = {
   fullName: string;
@@ -16,7 +12,6 @@ type FormData = {
 };
 
 export const CheckoutForm = () => {
-  const dispatch = useDispatch<AppDispatch>();
   const {
     register,
     handleSubmit,
@@ -24,10 +19,6 @@ export const CheckoutForm = () => {
   } = useForm<FormData>({
     resolver: zodResolver(CheckoutSchema),
   });
-
-  useEffect(() => {
-    dispatch(clearCart());
-  }, [dispatch]);
 
   const navigate = useNavigate();
 
--- src/page/CheckoutComplete.tsx
+++ src/page/CheckoutComplete.tsx
@@ -1,12 +1,17 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import OrderCompleteIllustration from "../components/ui/CheckoutSvg";
 import Confetti from "react-confetti";
-import { RootState } from "../store/store";
+import { AppDispatch, RootState } from "../store/store";
 import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { clearCart } from "../store/Cart/CartSlice";
 
 export const CheckoutComplete = () => {
   const { finalPrice } = useSelector((state: RootState) => state.cart);
-
+  const dispatch = useDispatch<AppDispatch>();
+  useEffect(() => {
+    dispatch(clearCart());
+  }, [dispatch]);
   if (finalPrice === 0) {
     return <Navigate to="/" />;
   }
